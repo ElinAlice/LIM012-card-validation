84a22d33e57361c621131768ecfd5141c629a9c9
ElinAlice <nelializ02@gmail.com>
2020-02-11 21:01:33 -0500

    Implementando funcion de validacion duplicando los numeros

--- pruebas/validator.js
+++ pruebas/validator.js
@@ -1,13 +1,35 @@
-  const isValid = () =>{
-    let numberCard = [1,2,3,4,8,9,3,5,6,7,,6,6,7,2,2];
+    const numberSum = (acumulator, currentValue ) => parse(acumulator) + parseInt(currentValue);
+    let arr = [2,3,4];
+    console.log(arr.reduce(numberSum));
+
+    const isValid = () =>{
+    let numberCard = [4,1,3,7,8,9,4,7,1,1,7,5,5,9,0,4];
     numberCard.reverse();
     console.log(numberCard);
 
-    for(i = 1 ; i <= numberCard.length; i++)
+    for(i = 0 ; i < numberCard.length; i++)
     {
-      console.log(i);
+      //console.log(i + ' '+ numberCard);
+      // obteniendo posicion
+      if(i % 2 !== 0)
+      {
+        
+        let doubleNumber = numberCard[i] * 2;
+        let converterString = doubleNumber.toString().split(',');
+        let digitSum = converterString.reduce(numberSum);
+        
+        console.log(digitSum);
+        
+        
+      }
+      /*if (i % 2 === 0)
+      {
+        
+        console.log(i+ ' '+ numberCard[i-1]);
+        //let doubleNumber = numberCard[i] * 2;
+        //console.log(doubleNumber);
+      }*/
     }
 };
-
 isValid();
 
