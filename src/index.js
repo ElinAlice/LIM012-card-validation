4ab92c4e23e647559af49ab840d26af0718a619b
ElinAlice <nelializ02@gmail.com>
2020-02-18 12:39:58 -0500

    Implementando css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
 import validator from './validator.js';
 
 const btnRegistry = document.getElementById('btnRegistry');
+const containerInvalidCard = document.getElementById('containerInvalidCard');
+const containerForm = document.getElementById('frmContainer');
+const containerVerifyRegistration = document.getElementById('containerVerifyRegistration');
+
+containerInvalidCard.style.display = 'none';
+containerForm.style.display = 'block';
+containerVerifyRegistration.style.display = 'none';
 
 btnRegistry.addEventListener('click', () => {
 
     let creditCardNumber = document.getElementById('txtCreditCardNumber').value;
-    console.log(creditCardNumber);
     let cardValidation = validator.isValid(creditCardNumber);
     let maskedCard = validator.maskify(creditCardNumber);
-    
+
+    let txtFullName = document.getElementById('txtFullName').value;
+    let txtAddress = document.getElementById('txtAddress').value;
+    let txtPhone = document.getElementById('txtPhone').value;
+    let txtEmail = document.getElementById('txtEmail').value;
+
     if(cardValidation === true)
     {
-        alert('Tu tarjeta es: ' + maskedCard);
+        containerVerifyRegistration.style.display = 'block';
+        containerForm.style.display = 'none';
+
+        let showFullName = document.getElementById('showFullName');
+        let showAddress = document.getElementById('showAddress');
+        let showPhone = document.getElementById('showPhone');
+        let showEmail = document.getElementById('showEmail');
+        let showCreditCardNumber = document.getElementById('showCreditCardNumber');
+
+        showFullName.innerHTML = txtFullName;
+        showAddress.innerHTML = txtAddress;
+        showPhone.innerHTML = txtPhone;
+        showEmail.innerHTML = txtEmail;
+        showCreditCardNumber.innerHTML = maskedCard;
     }
     else{
-        alert('Tu tarjeta es invalida vuelve a ingresarla');
+        containerInvalidCard.style.display = 'block';
+        creditCardNumber = '';
     }
 });
 
